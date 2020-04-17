18d5c25f133865d0103f70ae2437c8c4dea33e08
bartek9105 <bartek9105@gmail.com>
2020-04-17 11:40:32 +0200

    minor styling improvements

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="hero bg-indigo-500 flex items-center py-24 flex-col">
-      <h2 class="text-gray-100 text-3xl mb-6">Search for a job</h2>
-      <input type="text" placeholder="Search for a job" class="border-solid rounded-full outline-none py-2 px-4 w-2/5 shadow-lg">
+  <div class="hero bg-blue-600 flex items-center py-24 flex-col">
+      <h2 class="text-gray-100 text-3xl mb-6 tracking-wider">Search for a job</h2>
+      <input type="text" placeholder="Search" class="border-solid rounded-full outline-none py-2 px-4 w-2/5 shadow-lg">
   </div>
 </template>
 
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,18 +1,20 @@
 <template>
-    <header class="max-w-screen-xl mx-auto flex justify-between p-4 bg-indigo-700 text-gray-100 py-6 tracking-wider items-center">
-        <div>
-            <span class="mr-3 text-xl">CodeJob</span>
-            <i class="fas fa-briefcase text-xl"></i>
-        </div>
-        <nav>
-            <ul class="flex items-center">
-                <li class="mr-6">Login</li>
-                <li class="mr-6">Signup</li>
-                <li>
-                    <button class="bg-red-600 p-3 px-4 rounded-md outline-none hover:bg-red-800">Post a job</button>
-                </li>
-            </ul>
-        </nav>
+    <header class="mx-auto flex justify-between p-4 bg-blue-700 text-gray-100 py-6 tracking-wider items-center shadow-outline">
+            <div>
+                <span class="text-2xl tracking-widest">
+                    <span class="text-red-600">Code</span>Job
+                </span>
+                <i class="fas fa-briefcase text-xl"></i>
+            </div>
+            <nav>
+                <ul class="flex items-center">
+                    <li class="mr-6">Login</li>
+                    <li class="mr-6">Signup</li>
+                    <li>
+                        <button class="bg-red-600 p-3 px-4 rounded-md outline-none hover:bg-red-800">Post a job</button>
+                    </li>
+                </ul>
+            </nav>
     </header>
 </template>
 
