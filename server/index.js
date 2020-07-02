bcfdc7073efb63391367a175e77c7402b9381aae
bartek9105 <bartek9105@gmail.com>
2020-07-02 12:18:30 +0200

    server init

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const cors = require('cors')
 
+dotenv.config({ path: './config/config.env' })
+ 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Server started on ${port}`))
+app.listen(PORT, () => console.log(`Server started on ${PORT} in ${process.env.NODE_ENV} mode`))
