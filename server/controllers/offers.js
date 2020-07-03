b5aea6992d17d3ac8376b1a2501166a5165247f2
bartek9105 <bartek9105@gmail.com>
2020-07-03 11:49:24 +0200

    crud methods for offers

--- server/controllers/offers.js
+++ server/controllers/offers.js
@@ -1,19 +1,85 @@
-exports.getOffers = (req, res, next) => {
-    res.status(200).send('All offers')
+const Offer = require('../models/Offer')
+
+exports.getOffers = async (req, res, next) => {
+    try {
+        const offers = await Offer.find()
+        res.status(200).send({
+            count: offers.length,
+            data: offers
+        })
+    } catch (error) {
+        res.status(400).send({
+            error: error
+        })
+    }
 }
 
-exports.getOffer = (req, res, next) => {
-    res.status(200).send('Single offer')
+exports.getOffer = async (req, res, next) => {
+    try {
+        const offer = await Offer.findById(req.params.id)
+        res.status(200).send({
+            data: offer
+        })
+        if (!offer) {
+            return res.status(400).send({
+                error: error
+            })
+        }
+    } catch (error) {
+        res.status(400).send({
+            error: error
+        })
+    }
 }
 
-exports.addOffer = (req, res, next) => {
-    res.status(200).send('Add offer')
+exports.addOffer = async (req, res, next) => {
+    try {
+        const offer = new Offer(req.body)
+        const savedOffer = await offer.save()
+        res.status(200).send({
+            data: savedOffer
+        })        
+    } catch (error) {
+        res.status(400).send({
+            error: error
+        })
+    }
 }
 
-exports.editOffer = (req, res, next) => {
-    res.status(200).send('Edit offer')
+exports.editOffer = async (req, res, next) => {
+    try {
+        const offer = await Offer.findByIdAndUpdate(req.params.id, req.body, {
+            runValidators: true
+        })
+        if (!offer) {
+            return res.status(400).send({
+                error: error
+            })
+        }
+        res.status(200).send({
+            data: offer
+        })        
+    } catch (error) {
+        res.status(400).send({
+            error: error
+        })
+    }
 }
 
-exports.deleteOffer = (req, res, next) => {
-    res.status(200).send('Delete offer')
+exports.deleteOffer = async (req, res, next) => {
+    try {
+        const offer = await Offer.findByIdAndRemove(req.params.id)
+        if (!offer) {
+            return res.status(400).send({
+                error: error
+            })
+        }
+        res.status(200).send({
+            data: {}
+        })        
+    } catch (error) {
+        res.status(400).send({
+            error: error
+        })
+    }
 }
