76d7c9e7097a52707acc2618a6e3f618411d447e
bartek9105 <bartek9105@gmail.com>
2020-07-06 21:55:07 +0200

    change schema user name to employer, add new fields

---
+++ server/models/Employer.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const employerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a company name']
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an e-mail'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: 8,
+        select: false
+    },
+    size: {
+        type: String,
+        default: 'Company size not specified',
+        trim: true
+    },
+    industry: {
+        type: String,
+        default: 'Company industry not specified',
+        trim: true
+    },
+    about: {
+        type: String,
+        maxlength: [1000, 'About section cannot be longer than 1000 characters'],
+        trim: true
+    },
+    logo: {
+        type: String,
+        default: 'fas fa-building',
+        trim: true
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Employer', employerSchema)
