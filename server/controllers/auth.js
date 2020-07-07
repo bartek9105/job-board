7fcd7a18ea9a1f4c70bee39ee2421d0da8b78aef
bartek9105 <bartek9105@gmail.com>
2020-07-07 11:02:34 +0200

    create login route, login controller, sign jwt token after valid login

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const Employer = require('../models/Employer')
 
 exports.register = async (req, res, next) => {
@@ -12,8 +13,39 @@ exports.register = async (req, res, next) => {
         const user = await new Employer({
             name, email, password
         })
-        const savedUser = await user.save()
+        await user.save()
+
         res.status(200).send('User registered')
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return next(new ErrorResponse('Please provide an email and password', 401))
+        }
+        
+        const employer = await Employer.findOne({ email })
+        
+        if (!employer) {
+            return next(new ErrorResponse('Invalid credentials'), 401)
+        }
+        
+        const passwordValid = await bcrypt.compare(password, employer.password)
+
+        if (!passwordValid) {
+            return next(new ErrorResponse('Invalid credentials'), 401)
+        }
+
+        const token = jwt.sign({
+            id: employer._id
+        }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES })
+
+        res.status(200).send({ token })
     } catch (error) {
         next(error)
     }
