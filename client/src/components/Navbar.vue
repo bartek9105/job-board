6a29637250a479b72a972387a401ca717bf26644
bartek9105 <bartek9105@gmail.com>
2020-09-08 09:42:33 +0200

    change navbar styling

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,19 +1,22 @@
 <template>
-    <header class="mx-auto flex justify-between bg-indigo-900 text-gray-100 tracking-wider items-center shadow-outline px-8 py-4">
-            <Logo/>
-            <nav>
-                <ul class="flex items-center">
-                    <router-link to="/login">
-                        <li class="mr-6">Login</li>
-                    </router-link>
-                    <router-link to="/register">
-                        <li class="mr-6">Sign up</li>
-                    </router-link>
-                    <li>
-                        <button class="bg-red-600 p-3 px-4 rounded-md outline-none hover:bg-red-800">Post a job</button>
-                    </li>
-                </ul>
-            </nav>
+    <header class="mainHeader">
+        <Logo/>
+        <nav class="mainNav">
+            <ul>
+                <router-link to="/login">
+                    <li>Offers</li>
+                </router-link>
+                <router-link to="/login">
+                    <li>Login</li>
+                </router-link>
+                <router-link to="/register">
+                    <li>Sign up</li>
+                </router-link>
+                <li>
+                    <button>Post a job</button>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
@@ -27,6 +30,21 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+    .mainHeader {
+        max-width: 1640px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .mainNav {
+        ul {
+            display: flex;
+            li {
+                margin-right: 48px;
+                font-weight: 700;
+            }
+        }
+    }
 </style>
