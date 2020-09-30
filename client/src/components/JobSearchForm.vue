50ef4a7640bf38d73faa6970fc3efedbe28e3aa2
bartek9105 <bartek9105@gmail.com>
2020-09-30 13:41:32 +0200

    tags input positioning and styling fixes, take care of edge cases

--- client/src/components/JobSearchForm.vue
+++ client/src/components/JobSearchForm.vue
@@ -27,18 +27,20 @@
           <input type="number" placeholder='MIN' class="search-input salary-input">
           <input type="number" placeholder='MAX' class="search-input salary-input">
         </div>
-    </div>
-    <Button>Search</Button>
+      </div>
+      <Button>Search</Button>
     </div>
     <div class="single-input">
       <span class="input-name">Technologies</span>
       <div class="tag-input-container">
-        <div v-for="(tag, index) in technologyTags" :key="index" class="tag">
-          {{ tag }}
-          <i class="fas fa-times-circle delete-icon" @click="deleteTag(index)"></i>
+        <div class="tag-input-insider">
+          <div v-for="(tag, index) in technologyTags" :key="index" class="tag">
+            {{ tag }}
+            <i class="fas fa-times-circle delete-icon" @click="deleteTag(index)"></i>
+          </div>
+          <input type="text" class="tag-input" v-model="technology" @keyup.enter="createTechnologyTag" placeholder="Type in technology, press Enter to add more">
+          <i class="fas fa-times-circle delete-icon delete-all" v-if="technologyTags.length > 0" @click="deleteAllTags"></i>
         </div>
-        <input type="text" class="tag-input" v-model="technology" @keyup.enter="createTechnologyTag" placeholder="Type in technology, press Enter to add more">
-        <i class="fas fa-times-circle delete-icon delete-all" v-if="technologyTags.length > 0" @click="deleteAllTags"></i>
       </div>
     </div>
   </div>
@@ -75,63 +77,75 @@ export default {
 
 <style lang="scss">
 .job-search-container {
-  max-width: 1600px;
+  max-width: 1087px;
   margin: 0 auto;
+  .input-name {
+    text-transform: uppercase;
+    font-size: 15px;
+    font-weight: 700;
+    margin-bottom: 28px;
+  }
   .job-search {
     display: flex;
     justify-content: space-between;
     margin-bottom: 50px;
-  }
-  .single-input {
-    display: flex;
-    flex-direction: column;
-    .tag-input-container {
-      border: 1px solid $theme-light-blue;
-      border-radius: 5px;
-      padding: 5px 10px;
-      position: relative;
-      .delete-icon {
-        margin-left: 3px;
-        cursor: pointer;
-      }
-      .delete-all {
-        position: absolute;
-        top: 15px;
-        right: 10px;
-      }
-    }
-    .tag {
-      @include tag;
-      @include tag-dark;
-      display: inline-block;
-      margin-right: 5px;
-    }
-    .tag-input {
-      min-width: 300px;
-      border: none;
-      height: 35px;
-      outline: none;
-    }
-    .input-name {
-      text-transform: uppercase;
-      font-size: 15px;
-      font-weight: 700;
-      margin-bottom: 28px;
-    }
-    .salary-inputs {
+    .single-input {
       display: flex;
-      .salary-input {
-        width: 90px;
-        margin-right: 30px;
+      flex-direction: column;
+      .salary-inputs {
+        display: flex;
+        .salary-input {
+          width: 90px;
+          margin-right: 20px;
+        }
+      }
+      .search-input {
+        width: 200px;
+        height: 35px;
+        border: 1px solid $theme-light-blue;
+        border-radius: 5px;
+        padding: 0 10px;
       }
     }
   }
-  .search-input {
-    width: 200px;
-    height: 35px;
+  .tag-input-container {
+    max-width: 100%;
     border: 1px solid $theme-light-blue;
     border-radius: 5px;
-    padding: 0 10px;
+    padding: 5px 10px;
+    position: relative;
+    margin-top: 28px;
+    .delete-icon {
+      margin-left: 3px;
+      cursor: pointer;
+    }
+    .delete-all {
+      position: absolute;
+      top: 15px;
+      right: 15px;
+    }
+    .tag-input-insider {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      .tag {
+        @include tag;
+        @include tag-dark;
+        display: inline-block;
+        margin: 5px 5px 5px 0;
+      }
+      .tag-input {
+        min-width: 200px;
+        margin-right: 30px;
+        height: 30px;
+        outline: none;
+        border: none;
+        flex: 1;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+      }
+    }
   }
 }
 </style>
