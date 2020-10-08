d93acf169b146ea5c4cdbc867434e96e293d9891
bartek9105 <bartek9105@gmail.com>
2020-10-08 14:07:10 +0200

    add custom scrollbar for suggestion list

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,4 +5,11 @@
 </template>
 
 <style lang="scss">
+  ul::-webkit-scrollbar {
+    width: 0.5em;
+  }
+  ul::-webkit-scrollbar-thumb {
+    background-color: $theme-dark-blue;
+    border-radius: 5px;
+  }
 </style>
