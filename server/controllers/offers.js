3ced3ee9ddfe626ee737c19b5e763f9761597999
bartek9105 <bartek9105@gmail.com>
2020-12-01 14:38:08 +0100

    Feature: Sort offers list by date and promotion status - put promoted offers on top

--- server/controllers/offers.js
+++ server/controllers/offers.js
@@ -32,7 +32,7 @@ exports.getOffers = async (req, res, next) => {
         const availableFilters = Object.keys(Offer.schema.paths)
         const schemaFilters = _.pickBy(parsedQuery, (value, key) => availableFilters.indexOf(key) > -1)
         
-        const offers = await Offer.find({ ...schemaFilters, ...locationSearch, $or: [{ status: "free" }, { status: "paid" }] }).skip(startIndex).limit(limit)
+        const offers = await Offer.find({ ...schemaFilters, ...locationSearch, $or: [{ status: "free" }, { status: "paid" }] }).skip(startIndex).limit(limit).sort({ isPromoted: -1, createdAt: 'desc' })
         
         const total = await Offer.find({ ...schemaFilters, ...locationSearch }).countDocuments()
 
@@ -89,7 +89,7 @@ exports.getOffer = async (req, res, next) => {
 
 exports.addOffer = async (req, res, next) => {
     try {
-        if (req.body.price === 0) {
+        if (req.body.product[0].price === 0) {
             const offer = new Offer({ ...req.body, status: 'free', expireAt: setOfferExpiryDate(7) })
             await offer.save()
         }
