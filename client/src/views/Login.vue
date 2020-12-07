77b60a5435b1e114492fb9f52563224b1cd2a138
bartek9105 <bartek9105@gmail.com>
2020-12-07 13:12:58 +0100

    Feature: Create Login view using Auth component

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,10 +1,30 @@
 <template>
-    <div></div>
+    <div>
+        <Auth infoRoute="/register">
+            <template v-slot:header>
+                <h1>Login</h1>
+            </template>
+            <template v-slot:info>
+                Don't have an account?
+            </template>
+            <template v-slot:info-action>
+                Register
+            </template>
+            <template v-slot:btn-text>
+                Login
+            </template>
+        </Auth>
+    </div>
 </template>
 
 <script>
+import Auth from '@/components/Auth'
 
 export default {
+  name: 'Login',
+  components: {
+    Auth
+  }
 }
 </script>
 
