7b9cddcce6837e31c3f78886591feee554a475b5
bartek9105 <bartek9105@gmail.com>
2020-12-07 12:35:06 +0100

    Feature: Add social login buttons for register view

--- client/src/main.js
+++ client/src/main.js
@@ -3,12 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { faUserSecret, faLongArrowAltLeft, faLongArrowAltRight, faMapMarkerAlt, faHeart, faMoneyBillWave, faFileSignature, faClock, faCheckCircle, faSearch } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './utils/formValidationRules'
 import './assets/css/index.scss'
 
-library.add(faUserSecret, faLongArrowAltLeft, faLongArrowAltRight, faMapMarkerAlt, faHeart, faMoneyBillWave, faFileSignature, faClock, faCheckCircle, faSearch)
+library.add(faUserSecret, faLongArrowAltLeft, faLongArrowAltRight, faMapMarkerAlt, faHeart, faMoneyBillWave, faFileSignature, faClock, faCheckCircle, faSearch, faFacebook, faGoogle)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -8,11 +8,11 @@
             <form @submit.prevent class="register__form">
                 <div class="form-unit">
                     <label for="email" class="register__form__label">E-mail</label>
-                    <input type="email" name="email">
+                    <input type="email" name="email" placeholder="Type in your e-mail address">
                 </div>
                 <div class="form-unit">
                     <label for="password" class="register__form__label">Password</label>
-                    <input type="password" name="password">
+                    <input type="password" name="password" placeholder="Type in your password">
                 </div>
                 <span class="register__form__info">
                     Already have an account?
@@ -21,6 +21,18 @@
                 <router-link to="/">
                     <Button>Register</Button>
                 </router-link>
+                <div class="register__form__alt">
+                    <hr>
+                    <span>Or</span>
+                    <button class="register__form__alt__btn register__form__alt__btn--facebook">
+                        <font-awesome-icon :icon="['fab', 'facebook']" class="register__form__alt__btn__icon"/>
+                        Sign in with Facebook
+                    </button>
+                    <button class="register__form__alt__btn register__form__alt__btn--google">
+                        <font-awesome-icon :icon="['fab', 'google']" class="register__form__alt__btn__icon"/>
+                        Sign in with Google
+                    </button>
+                </div>
             </form>
         </div>
     </div>
@@ -56,11 +68,47 @@ export default {
                 margin-bottom: $margin-sm;
             }
             &__info {
+                color: $dark-blue;
                 font-size: $font-content-md;
                 font-weight: $font-bold;
                 margin-bottom: $margin-md;
                 &__login {
                     color: $pink;
+                }
+            }
+            &__alt {
+                @include flex(null, center, column);
+                margin-top: $margin-md;
+                hr {
+                    height: 1px;
+                    background-color: $light-blue;
+                    border: 0;
+                    width: 100%;
+                    margin: $margin-sm 0;
+                }
+                &__btn {
+                    @include flex(center, center);
+                    width: 250px;
+                    color: $white;
+                    padding: $padding-sm;
+                    font-size: $font-content-md;
+                    border: $input-select-border;
+                    border-radius: $border-radius-sm;
+                    margin-top: $margin-sm;
+                    &--facebook {
+                        background-color: $facebook;
+                    }
+                    &--google {
+                        background-color: $google;
+                    }
+                    &__icon {
+                        font-size: $font-content-lg;
+                        margin-right: 1rem;
+                    }
+                    &:hover {
+                        @include shadow-hover;
+                        @include transition;
+                    }
                 }
             }
         }
