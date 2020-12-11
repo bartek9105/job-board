06b726ceb42c57c79b4317af70cc6106cb63c5af
bartek9105 <bartek9105@gmail.com>
2020-12-11 11:13:00 +0100

    Feature: Create middleware to check if cookie jwt token is valid

---
+++ server/middleware/isAuth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+exports.isAuth = async (req, res, next) => {
+    let token
+    if (req.headers.cookie) {
+        const cookies = req.headers.cookie.split('; ')
+        token = cookies.find(cookie => cookie.includes('token'))    
+        if (token) {
+            token = token.split('=')[1]
+            try {
+                const decodedToken = await jwt.verify(token, process.env.JWT_SECRET)
+                req.userId = decodedToken.id
+                next()
+            } catch (error) {
+                console.log(error)
+            }
+        }
+    }
+}
