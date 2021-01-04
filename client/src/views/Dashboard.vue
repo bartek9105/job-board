ea05428b54f7ece186496e3b79624bf4bc44a383
bartek9105 <bartek9105@gmail.com>
2021-01-04 14:26:26 +0100

    Feature: Create store logic for fetching offers by user id

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -6,11 +6,21 @@
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
 import Navbar from '@/components/Navbar'
 export default {
   name: 'Dashboard',
   components: {
     Navbar
+  },
+  computed: {
+    ...mapGetters(['getOffersByUser'])
+  },
+  mounted() {
+    this.fetchOffersByUser()
+  },
+  methods: {
+    ...mapActions(['fetchOffersByUser'])
   }
 }
 </script>
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,7 +2,7 @@ const Offer = require('../models/Offer')
 
 exports.getUserOffers = async (req, res, next) => {
   try {
-    const offers = await Offer.find({ creator: req.creatorId })
+    const offers = await Offer.find({ creator: req.params.id })
     res.send(offers)
   } catch (error) {
     next(error)
