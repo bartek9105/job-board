0e5c305c5c40353dbb94ff96e4a8a79c0fbf23bb
bartek9105 <bartek9105@gmail.com>
2021-01-11 14:01:21 +0100

    Refactor: Use object destructuring for offers route methods

--- server/routes/offers.route.js
+++ server/routes/offers.route.js
@@ -9,31 +9,21 @@ const {
   deleteOffer,
   updateOfferStatus,
 } = require('../controllers/offer.controller.js')
-const paymentController = require('../controllers/payment.controller')
-const productController = require('../controllers/products')
 
-const isAuth = require('../middleware/isAuth')
+const {
+  createPaymentSession,
+  listenForPaymentSuccess,
+} = require('../controllers/payment.controller')
+
+const { getProduct } = require('../controllers/products')
+
+const { isAuth } = require('../middleware/isAuth')
 
 router.get('/', getOffers)
-
 router.get('/:id', getOffer)
-
-router.post(
-  '/',
-  isAuth.isAuth,
-  productController.getProduct,
-  addOffer,
-  paymentController.createPaymentSession
-)
-
-router.post(
-  '/webhook',
-  paymentController.listenForPaymentSuccess,
-  updateOfferStatus
-)
-
-router.put('/:id', isAuth.isAuth, editOffer)
-
-router.delete('/:id', isAuth.isAuth, deleteOffer)
+router.post('/', isAuth, getProduct, addOffer, createPaymentSession)
+router.post('/webhook', listenForPaymentSuccess, updateOfferStatus)
+router.put('/:id', isAuth, editOffer)
+router.delete('/:id', isAuth, deleteOffer)
 
 module.exports = router
