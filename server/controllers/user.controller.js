182ba3dfae45f20c6ddfbf5c8aa2e61d53ccf3b7
bartek9105 <bartek9105@gmail.com>
2021-01-17 16:36:28 +0100

    Feature: Server logic for uploading users avatars using google-cloud-storage sdk

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const { getUserOffers } = require('../services/user.service')
+const { getUserOffers, uploadUserAvatar } = require('../services/user.service')
 
 exports.getUserOffers = async (req, res, next) => {
   const userId = req.params.id
@@ -6,6 +6,16 @@ exports.getUserOffers = async (req, res, next) => {
     const offers = await getUserOffers(userId)
     res.send(offers)
   } catch (error) {
+    next(error)
+  }
+}
+
+exports.uploadFile = async (req, res, next) => {
+  try {
+    const url = await uploadUserAvatar(req.files.foo)
+    res.status(200).send(url)
+  } catch (error) {
+    console.log(error)
     next(error)
   }
 }
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,9 +1,30 @@
 const Offer = require('../models/Offer')
+const { Storage } = require('@google-cloud/storage')
 
 const UserService = {
   getUserOffers: async function (userId) {
     try {
       return await Offer.find({ creator: userId })
+    } catch (error) {
+      console.log(error)
+    }
+  },
+  uploadUserAvatar: async function ({ name, mimetype, data }) {
+    const storage = new Storage({
+      keyFilename: './config/google-credentials.json',
+    })
+    try {
+      const bucket = await storage.bucket(process.env.GCS_BUCKET)
+      const blob = bucket.file(name)
+      const stream = blob.createWriteStream({
+        metadata: {
+          contentType: mimetype,
+        },
+      })
+
+      stream.on('error', (err) => console.log(err))
+      stream.end(data)
+      return `https://storage.cloud.google.com/${process.env.GCS_BUCKET}/${name}`
     } catch (error) {
       console.log(error)
     }
