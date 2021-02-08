4375b222993c623ee139359937f6e85f58738ad2
bartek9105 <bartek9105@gmail.com>
2021-02-08 11:48:08 +0100

    Feature: Add styling for company item component

---
+++ client/src/components/Base/BaseCompanyItem.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="company-item">
+    <BaseCompanyLogo
+      :avatar-url="company.avatarUrl"
+      img-width="250"
+      img-height="200"
+    />
+    <div class="company-item__description">
+      <h3 class="company-item__description__company-name">
+        {{ company.name }}
+      </h3>
+      <div class="company-item__description__info">
+        <font-awesome-icon
+          icon="industry"
+          class="company-item__description__info__icon"
+        />
+        <span>{{ company.industry }}</span>
+      </div>
+      <div class="company-item__description__info">
+        <font-awesome-icon
+          icon="user"
+          class="company-item__description__info__icon"
+        />
+        <span>{{ company.size }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import BaseCompanyLogo from '@/components/Base/BaseCompanyLogo'
+
+export default {
+  name: 'BaseCompanyItem',
+  components: {
+    BaseCompanyLogo
+  },
+  props: {
+    company: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.company-item {
+  @include shadow;
+  max-width: 300px;
+  background-color: $white;
+  &:hover {
+    @include shadow-hover;
+    cursor: pointer;
+  }
+  &__description {
+    padding: $padding-sm;
+    &__company-name {
+      margin-bottom: $margin-sm;
+    }
+    &__info {
+      font-size: $font-content-lg;
+      margin-bottom: $margin-xsm;
+      &__icon {
+        margin-right: 1rem;
+      }
+    }
+  }
+}
+</style>
--- client/src/views/Company/CompaniesList.vue
+++ client/src/views/Company/CompaniesList.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>Companies List</div>
+  <div>
+    <BaseHero />
+    <div class="company-items-container">
+      <div class="company-items">
+        <div v-for="company in getUsers" :key="company._id">
+          <BaseCompanyItem :company="company" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import BaseCompanyItem from '@/components/Base/BaseCompanyItem'
+import BaseHero from '@/components/Base/BaseHero'
 
 export default {
   name: 'CompaniesList',
+  components: {
+    BaseCompanyItem,
+    BaseHero
+  },
   methods: {
     ...mapActions(['fetchUsers'])
   },
@@ -19,4 +34,18 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.company-items-container {
+  width: 100%;
+  background: $bg-grey;
+  padding: $padding-lg 0;
+}
+.company-items {
+  max-width: $container-width;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+}
+</style>
