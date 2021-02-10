1597447fd9bf8ab4b5f6581fba2d04622ac7479f
bartek9105 <bartek9105@gmail.com>
2021-02-10 13:53:56 +0100

    Refactor: Use container component for footer

--- client/src/components/TheFooter.vue
+++ client/src/components/TheFooter.vue
@@ -1,46 +1,48 @@
 <template>
-  <footer>
-    <div class="footer">
-      <BaseLogo :job-text-color="jobTextColor" />
-      <div class="footer__navs-container">
-        <nav class="footer-nav">
-          <ul>
-            <li>About us</li>
-            <li>Career</li>
-            <li>Contact</li>
-            <li>Blog</li>
-          </ul>
-        </nav>
-        <nav class="footer-nav">
-          <ul>
-            <li>Help</li>
-            <li>Cookies</li>
-            <li>Terms & conditions</li>
-            <li>Privacy Policy</li>
-          </ul>
-        </nav>
-      </div>
-      <div class="footer__socials">
-        <span>Follow us</span>
-        <div class="footer__socials-container">
-          <a href="#">
-            <i class="social-icon fab fa-linkedin" />
-          </a>
-          <a href="#">
-            <i class="social-icon fab fa-facebook" />
-          </a>
-          <a href="#">
-            <i class="social-icon fab fa-instagram" />
-          </a>
-          <a href="#">
-            <i class="social-icon fab fa-github" />
-          </a>
-          <a href="#">
-            <i class="social-icon fas fa-globe-americas" />
-          </a>
+  <footer class="footer">
+    <Container>
+      <div class="footer-container">
+        <BaseLogo :job-text-color="jobTextColor" />
+        <div class="footer__navs-container">
+          <nav class="footer-nav">
+            <ul>
+              <li>About us</li>
+              <li>Career</li>
+              <li>Contact</li>
+              <li>Blog</li>
+            </ul>
+          </nav>
+          <nav class="footer-nav">
+            <ul>
+              <li>Help</li>
+              <li>Cookies</li>
+              <li>Terms & conditions</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </nav>
+        </div>
+        <div class="footer__socials">
+          <span>Follow us</span>
+          <div class="footer__socials-container">
+            <a href="#">
+              <font-awesome-icon icon="linkedin" />
+            </a>
+            <a href="#">
+              <i class="social-icon fab fa-facebook" />
+            </a>
+            <a href="#">
+              <i class="social-icon fab fa-instagram" />
+            </a>
+            <a href="#">
+              <i class="social-icon fab fa-github" />
+            </a>
+            <a href="#">
+              <i class="social-icon fas fa-globe-americas" />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
     <div class="footer-bottom">
       <span class="footer-bottom__text">2020 CodeJob. All rights reserved</span>
     </div>
@@ -49,11 +51,13 @@
 
 <script>
 import BaseLogo from '@/components/Base/BaseLogo'
+import Container from '@/components/Base/UIContainers/Container'
 
 export default {
   name: 'TheFooter',
   components: {
-    BaseLogo
+    BaseLogo,
+    Container
   },
   data() {
     return {
@@ -65,12 +69,7 @@ export default {
 
 <style lang="scss">
 .footer {
-  @include flex(space-between, null);
-  background: $white;
-  font-size: $font-content-lg;
-  margin: $margin-center;
-  max-width: $container-width;
-  padding: $padding-lg 0;
+  background-color: $white;
   &__navs-container {
     @include flex();
     .footer-nav {
@@ -92,6 +91,10 @@ export default {
       }
     }
   }
+}
+.footer-container {
+  @include flex(space-between, null);
+  font-size: $font-content-lg;
 }
 .footer-bottom {
   &__text {
