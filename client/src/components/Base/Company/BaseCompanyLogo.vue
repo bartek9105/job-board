9e2ecd0aee4ba43290d2f3388b37ab031df4986b
bartek9105 <bartek9105@gmail.com>
2021-02-12 10:49:53 +0100

    Feature: Add ripple animation effect on loading for logo image

--- client/src/components/Base/Company/BaseCompanyLogo.vue
+++ client/src/components/Base/Company/BaseCompanyLogo.vue
@@ -3,7 +3,14 @@
     class="company-logo-container"
     :style="{ width: imgWidth + 'px', height: imgHeight + 'px' }"
   >
-    <img :src="avatarUrl" alt="logo" class="company-logo" />
+    <img
+      v-show="isImageLoaded"
+      :src="avatarUrl"
+      alt="logo"
+      class="company-logo"
+      @load="loaded"
+    />
+    <div v-show="!isImageLoaded" class="skeleton" />
   </div>
 </template>
 
@@ -14,6 +21,16 @@ export default {
     avatarUrl: String,
     imgWidth: Number,
     imgHeight: Number
+  },
+  data() {
+    return {
+      isImageLoaded: false
+    }
+  },
+  methods: {
+    loaded() {
+      this.isImageLoaded = true
+    }
   }
 }
 </script>
@@ -30,5 +47,39 @@ export default {
   width: 100%;
   height: auto;
   border-radius: 5px;
+}
+
+.skeleton {
+  @include shadow();
+  border-radius: $border-radius-sm;
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: -150px;
+    top: 0;
+    height: 100%;
+    width: 150px;
+    background: linear-gradient(
+      to right,
+      transparent 0%,
+      $dark-blue-lighter 50%,
+      transparent 100%
+    );
+    animation: load 1s cubic-bezier(0.4, 0, 0.2, 1) infinite;
+  }
+
+  @keyframes load {
+    from {
+      left: -150px;
+    }
+    to {
+      left: 100%;
+    }
+  }
 }
 </style>
