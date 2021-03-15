a0bf8658222192676bfa31dcd35658bd29ef9615
bartek9105 <bartek9105@gmail.com>
2021-03-15 16:13:52 +0100

    Feature: Use user info form in profile account view

--- client/src/views/Profile/ProfileAccount.vue
+++ client/src/views/Profile/ProfileAccount.vue
@@ -1,11 +1,40 @@
 <template>
-  <h1>Profile Account</h1>
+  <div class="dashboard-container">
+    <h1>Account</h1>
+    <UserInfoForm :user-info="getUserInfo" :save-offer="updateUser" />
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+import UserInfoForm from '@/components/Forms/UserInfoForm'
+
 export default {
-  name: 'ProfileAccount'
+  name: 'ProfileAccount',
+  components: {
+    UserInfoForm
+  },
+  computed: {
+    ...mapGetters(['getUserInfo'])
+  },
+  methods: {
+    ...mapActions(['updateUserInfo']),
+    updateUser() {
+      this.updateUserInfo(this.getUserInfo)
+    }
+  }
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.dashboard-container {
+  @include shadow;
+  background-color: $white;
+  border-radius: $border-radius-sm;
+  padding: $padding-lg;
+  h1 {
+    font-weight: $font-semi-bold;
+    margin-bottom: $margin-lg;
+  }
+}
+</style>
