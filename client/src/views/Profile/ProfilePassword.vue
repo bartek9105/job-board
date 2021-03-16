8a94e98fa5d0c9181b74a3751ff32212f40d2c61
bartek9105 <bartek9105@gmail.com>
2021-03-16 09:51:44 +0100

    Feature: Create profile password change component

--- client/src/views/Profile/ProfilePassword.vue
+++ client/src/views/Profile/ProfilePassword.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Password</h1>
+  <div class="dashboard-container">
+    <h1>Password</h1>
+    <form class="password-form" @submit.prevent>
+      <div class="password-form__unit">
+        <label for="old-password" class="password-form__label"
+          >Old password</label
+        >
+        <input type="password" name="old-password" />
+      </div>
+      <div class="password-form__unit">
+        <label for="new-password" class="password-form__label"
+          >New password</label
+        >
+        <input type="password" name="new-password" />
+      </div>
+      <BaseButton class="password-form__btn">
+        Save
+      </BaseButton>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -8,4 +27,30 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.dashboard-container {
+  @include shadow;
+  width: 700px;
+  background-color: $white;
+  border-radius: $border-radius-sm;
+  padding: $padding-lg;
+  h1 {
+    font-weight: $font-semi-bold;
+    margin-bottom: $margin-lg;
+  }
+}
+.password-form {
+  max-width: 1000px;
+  &__unit {
+    @include flex(null, null, column);
+    margin-bottom: $margin-sm;
+  }
+  &__label {
+    @include input-name;
+    margin-bottom: $margin-sm;
+  }
+  &__btn {
+    margin-top: $margin-sm;
+  }
+}
+</style>
