0113907eb914e7397cf661831dbafa807c7befdb
bartek9105 <bartek9105@gmail.com>
2021-03-29 11:57:58 +0200

    Feature: Minor styling improvements for file uploader

--- client/src/components/ImageUploadInput.vue
+++ client/src/components/ImageUploadInput.vue
@@ -3,18 +3,26 @@
     id="dropzone"
     ref="myVueDropzone"
     :options="dropzoneOptions"
+    :use-custom-slot="true"
     @vdropzone-file-added="getFile"
-  />
+  >
+    <div class="upload-message">
+      <upload-cloud-icon size="1.5x" class="upload-message__icon" /> Drop avatar
+      here to upload
+    </div>
+  </vue-dropzone>
 </template>
 
 <script>
 import vue2Dropzone from 'vue2-dropzone'
 import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+import { UploadCloudIcon } from 'vue-feather-icons'
 
 export default {
   name: 'ImageUploadInput',
   components: {
-    vueDropzone: vue2Dropzone
+    vueDropzone: vue2Dropzone,
+    UploadCloudIcon
   },
   data() {
     return {
@@ -34,14 +42,14 @@ export default {
 </script>
 
 <style lang="scss">
-.dropzone {
-  width: 200px;
-  padding: 0;
+.vue-dropzone {
+  width: 100%;
+  padding: $padding-sm;
   .dz-preview {
     width: 100%;
     margin: 0;
     .dz-remove {
-      width: 90%;
+      width: 50%;
       left: 0;
       right: 0;
       margin: 0 auto;
@@ -50,6 +58,19 @@ export default {
       background-color: $dark-blue;
       opacity: 0;
     }
+    .dz-image {
+      @include flex(center);
+      padding: $padding-sm;
+    }
   }
+}
+.upload-message {
+  @include flex(null, center);
+  &__icon {
+    margin-right: 1rem;
+  }
+}
+.dropzone.dz-clickable {
+  @include flex(center, center);
 }
 </style>
