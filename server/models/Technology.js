d417c427700ff48a65591994011f5822a978e654
bartek9105 <bartek9105@gmail.com>
2021-04-01 13:34:48 +0200

    Refactor: Delete unnecessary files

--- server/models/Technology.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const technologySchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-})
-
-module.exports = mongoose.model('Technology', technologySchema)
